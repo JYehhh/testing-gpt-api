35e44a8b791a014df76ca050466f3691c72884c7
Jerry Yeh <yehjerry2003@gmail.com>
2023-04-28 12:33:40 +1000

    added initial testing file

---
+++ testing.py
@@ -0,0 +1,6 @@
+import os
+import openai
+openai.organization = "org-gsZAkbOAlDD7mmMf5eF7HoTv"
+openai.api_key = os.getenv("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")
+openai.Model.list()
+
